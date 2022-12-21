4656f76cfd0d6dd0e90278e198d6d25a1fedb902
msuleman0100 <msuleman0100@gmail.com>
2022-12-21 18:18:13 +0500

    Completed

---
+++ MilestoneProgressView/MilestoneProgressView/MilestoneProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneProgressViewApp.swift
+//  MilestoneProgressView
+//
+//  Created by Muhammad Suleman on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
